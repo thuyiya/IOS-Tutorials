212a3c6bf9f5da9b1e99c8c960a39e34141bcb30
thusitha <thusitha.4t@gmail.com>
2020-02-01 11:58:22 +0530

    Add core data

--- FebFirstCoreData/FebFirstCoreData/ViewController.swift
+++ FebFirstCoreData/FebFirstCoreData/ViewController.swift
@@ -7,11 +7,12 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
     
-    var people: [String] = []
-
+    var people: [NSManagedObject] = []
+    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
@@ -20,13 +21,73 @@ class ViewController: UIViewController {
         // register the cell
         title = "Name List"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-        
-        people.append("Kamal")
-        tableView.reloadData()
     }
-
-    @IBAction func addName(_ sender: UIBarButtonItem) {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
+        //1
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        //2
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "People")
+        
+        //3
+        do {
+            people = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+    @IBAction func addName(_ sender: UIBarButtonItem) {
+        //alert ref
+        let alert = UIAlertController(title: "New Name", message: "Add a new name", preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) {[unowned self] action in
+            
+            guard let textField = alert.textFields?.first, let nameToSave = textField.text
+                else {
+                    return
+            }
+            
+            self.save(name: nameToSave)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField()
+        alert.addAction(saveAction)
+        present(alert, animated: true)
+    }
+    
+    func save(name: String) {
+        //init
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        // 1
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        // 2
+        let entity = NSEntityDescription.entity(forEntityName: "People", in: managedContext)!
+        
+        let person = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        // 3
+        person.setValue(name, forKeyPath: "name")
+        
+        // 4
+        do {
+            try managedContext.save()
+            people.append(person)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
     }
     
 }
@@ -40,40 +101,45 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        
-        cell.textLabel?.text = people[indexPath.row]
-        
+//
+//        cell.textLabel?.text = people[indexPath.row]
+//
+//        return cell
+
+        let person = people[indexPath.row]
+
+        cell.textLabel?.text = person.value(forKeyPath: "name") as? String
         return cell
     }
     
     
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        //        return names.count
-//        return people.count
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        // let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-//        //        cell.textLabel?.text = names[indexPath.row]
-//        //        return cell
-//
-//        let person = people[indexPath.row]
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-//        cell.textLabel?.text = person.value(forKeyPath: "name") as? String
-//        return cell
-//    }
-//
-//    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-//        return true
-//    }
-//
-//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//        if (editingStyle == .delete) {
-//            let person = people[indexPath.row]
-//            people.remove(at: indexPath.row)
-//            deleteData(name: (person.value(forKeyPath: "name") as? String)!)
-//            tableView.deleteRows(at: [indexPath], with: .fade)
-//        }
-//    }
+    //    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //        //        return names.count
+    //        return people.count
+    //    }
+    //
+    //    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //        // let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    //        //        cell.textLabel?.text = names[indexPath.row]
+    //        //        return cell
+    //
+    //        let person = people[indexPath.row]
+    //        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    //        cell.textLabel?.text = person.value(forKeyPath: "name") as? String
+    //        return cell
+    //    }
+    //
+    //    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    //        return true
+    //    }
+    //
+    //    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    //        if (editingStyle == .delete) {
+    //            let person = people[indexPath.row]
+    //            people.remove(at: indexPath.row)
+    //            deleteData(name: (person.value(forKeyPath: "name") as? String)!)
+    //            tableView.deleteRows(at: [indexPath], with: .fade)
+    //        }
+    //    }
 }
 
